b78fea1bac69f0e942065df28eddb85d499d47df
Jawahar B <90580495+JawaharBalu@users.noreply.github.com>
2023-12-18 17:38:17 -0500

    login-popup-complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import OfferCarousel from "./components/carousel/Carousel";
@@ -8,6 +8,7 @@ import LoginPopup from "./components/login/login-popup/LoginPopup";
 
 function App() {
   const [mode, setMode] = useState("light");
+  const [loginPopup, setLoginPopup] = useState(false);
   const electronicsBest = {
     name: "Best of Electronics",
     products: [
@@ -53,8 +54,72 @@ function App() {
       },
     ],
   };
+  const fashionBest = {
+    name: "Fashion Best Sellers",
+    products: [
+      {
+        group: "Safari wildcraft",
+        img: "https://rukminim1.flixcart.com/image/280/280/jwgple80/backpack/n/e/w/spin-laptop-backpack-01-teal-fs0-0-11-001-laptop-backpack-original-imafh54gqnmpsczs.jpeg?q=80",
+        offer: "Min. 65% off",
+      },
+      {
+        group: "Men's Sweatshirts",
+        img: "https://rukminim1.flixcart.com/image/380/380/xif0q/sweatshirt/w/d/j/s-hood1-do-four-lines-n-fashion-and-youth-original-imaghk5gxfcjfzyp.jpeg?q=80",
+        offer: "From ₹599",
+      },
+      {
+        group: "Trendy Kurtas",
+        img: "https://rukminim1.flixcart.com/fk-p-flap/280/280/image/dca97fb4b0c593f8.jpg?q=80",
+        offer: "Min 75% off!",
+      },
+      {
+        group: "Anmi, Divastri and more",
+        img: "https://rukminim1.flixcart.com/image/280/280/xif0q/sari/8/6/w/free-sknt17-d-divastri-unstitched-original-imagg9j9vvv8s3hb.jpeg?q=80",
+        offer: "Min. 60-90% off!",
+      },
+      {
+        group: "Leggings combo",
+        img: "https://rukminim1.flixcart.com/image/170/170/klo27bk0/churidar/n/x/k/xxl-women-churidar-nakaash-original-imagyqy6evhh7xzc.jpeg?q=80 ",
+        offer: "From ₹299",
+      },
+      {
+        group: "Dresses, Tops & Jeans",
+        img: "https://rukminim1.flixcart.com/image/280/280/xif0q/dress/j/s/f/s-kbt-559-krati-by-tara-original-imagg9hftvfxe56u.jpeg?q=80 ",
+        offer: "Min. 60% off",
+      },
+      {
+        group: "Max & Rangmanch",
+        img: "https://rukminim1.flixcart.com/image/280/280/kuef2q80/kurta/h/n/y/s-dafksp20pi21aqua-max-original-imag7j8gnghzahsu.jpeg?q=80",
+        offer: "Mn. ₹499",
+      },
+      {
+        group: "Sweatshirts & Hoodies",
+        img: "https://rukminim1.flixcart.com/image/280/280/k2z1t3k0/jacket/z/v/x/m-9298233-mast-harbour-original-imafm78vbfvdzph7.jpeg?q=80 ",
+        offer: "Under ₹299",
+      },
+    ],
+  };
+  // const fashionBest = {
+  //   name: "Fashion Best Sellers",
+  //   products: [
+  //     { group: "", img: "", offer: "" },
+  //     { group: "", img: "", offer: "" },
+  //     { group: "", img: "", offer: "" },
+  //     { group: "", img: "", offer: "" },
+  //     { group: "", img: "", offer: "" },
+  //     { group: "", img: "", offer: "" },
+  //     { group: "", img: "", offer: "" },
+  //     { group: "", img: "", offer: "" },
+  //   ],
+  // };
+  useEffect(() => {
+    setTimeout(() => {
+      setLoginPopup(true);
+    }, 4000);
+  }, []);
   return (
     <div className="container">
+      {loginPopup && <LoginPopup setLoginPopup={setLoginPopup} />}
       <header className="navbar">
         <Navbar mode={mode} setMode={setMode} />
       </header>
@@ -69,8 +134,8 @@ function App() {
         </div>
         <div className="categoriesBest">
           <ProductsSlide data={electronicsBest} />
+          <ProductsSlide data={fashionBest} />
         </div>
-        <LoginPopup/>
       </div>
     </div>
   );
--- src/components/login/login-popup/LoginPopup.jsx
+++ src/components/login/login-popup/LoginPopup.jsx
@@ -1,35 +1,52 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react/no-unescaped-entities */
 import "./LoginPopup.css";
-const LoginPopup = () => {
+const LoginPopup = ({ setLoginPopup }) => {
   return (
-    <div className="popupLayout">
-      <div className="leftPanel">
-        <span style={{ fontSize: "28px", fontWeight: 500, color: "#fff" }}>
-          Login
-        </span>
-        <span className="description">
-          Get access to your Orders, Wishlist and Recommendations
-        </span>
-      </div>
-      <div className="rightPanel">
-        <div className="loginFormLayout">
-          <form className="loginForm">
-            <input placeholder="Enter Email/Mobile number" />
-          </form>
-        </div>
-        <div  className="disclaimerDiv">
-          By continuing, you agree to Flipkart's{" "}
-          <a  target="_blank" href="/pages/terms">
-            Terms of Use
-          </a>{" "}
-          and{" "}
-          <a target="_blank" href="/pages/privacypolicy">
-            Privacy Policy
-          </a>
-          .
-        </div>
-        <div className="OTPButton">
-        <button>Request OTP</button>
+    <div className="popupContainer" >
+      <div
+        style={{
+          position: "relative",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <button className="closeButton" onClick={() => setLoginPopup(false)}>
+          X
+        </button>
+        <div className="popupLayout" >
+          <div className="leftPanel">
+            <span style={{ fontSize: "28px", fontWeight: 500, color: "#fff" }}>
+              Login
+            </span>
+            <span className="description">
+              Get access to your Orders, Wishlist and Recommendations
+            </span>
+          </div>
+          <div className="rightPanel">
+            <div className="loginFormLayout">
+              <form className="loginForm">
+                <input placeholder="Enter Email/Mobile number" />
+              </form>
+            </div>
+            <div className="disclaimerDiv">
+              By continuing, you agree to Flipkart's{" "}
+              <a target="_blank" href="/pages/terms">
+                Terms of Use
+              </a>{" "}
+              and{" "}
+              <a target="_blank" href="/pages/privacypolicy">
+                Privacy Policy
+              </a>
+              .
+            </div>
+            <div className="OTPButton">
+              <button>Request OTP</button>
+            </div>
+            <div className="signUpDiv">
+              <button>New to Flipkart? Create an account</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
